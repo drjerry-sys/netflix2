59bb1c52510e89e89dabaec5067d7fd7b2f42d2f
Hi there! I'm Jerry <64374109+drjerry-sys@users.noreply.github.com>
2022-01-27 00:21:52 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Watch />
+      <Register />
       <Routes>
         <Route path="/" app={Home} />
         <Route path="/login" app={Login} />
